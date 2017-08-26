ba134234565ebd6d2cc4c9fd6fda3a65c2b14b3a
Liam Miller <imasilverfalcon@gmail.com>
2017-08-26 11:33:39 -0400

    Added board.cpp and skeleton classes. Commented board.h

---
+++ board.cpp
@@ -0,0 +1,22 @@
+#include "board.h"
+
+
+Board::Board()
+{
+  this.layout = new Piece[9];
+}
+
+Piece[] Board::GetLayout(void)
+{
+    return NULL;
+}
+
+void Board::Move(Piece player, int index)
+{
+    
+}
+
+int Board::Status(void)
+{
+  return 0;
+}
--- board.h
+++ board.h
@@ -10,8 +10,19 @@ PIECE_X = 1
 class Board
 {
 public:
+//Constructor
+Board(void);
+/*@brief Gets the layout of the current board
+ *
+ *@return A Piece[] representing the current layout
+ */
 Piece[] GetLayout();
 
+/*@brief Changes a tile at the given index to the given Piece
+ *
+ *@param player [in] - The Piece representing whose turn it is
+ *@param index [in] - The position where we are placing the peice
+ */
 void Move(Piece player, int index);
 
 /*@brief Returns the status of the current board
